ef51bfdf16fd75dc0911448eb68b347ed41e2690
iratxe-pm <iratxepm04@gmail.com>
2025-06-25 13:31:24 +0200

    funciona

---
+++ main.py
@@ -0,0 +1,62 @@
+import tkinter as tk
+from PIL import Image, ImageDraw
+import numpy as np
+from keras.models import load_model
+
+
+red = load_model("red.keras")
+
+# Crear ventana en python
+ventana = tk.Tk()
+ventana.title("Dibuja un número")
+
+# Canvas
+canvas = tk.Canvas(ventana, width=280, height=280, bg='black')
+canvas.pack()
+
+# Imagen interna para guardar el dibujo
+imagen = Image.new("L", (28, 28), 0)
+dibujar = ImageDraw.Draw(imagen)
+
+def dibujar_con_mouse(event):
+    x = event.x
+    y = event.y
+    r = 6 #radio del pincel
+    #crea un cículo con el centro en la coordenada donde está el puntero
+    canvas.create_oval(x - r, 
+                       y - r, 
+                       x + r, 
+                       y + r, 
+                       fill="white", outline="white")
+    # Escalar coordenadas a 28x28
+    x_i = event.x * 28 // 280
+    y_i = event.y * 28 // 280
+    dibujar.ellipse([x_i - 1, y_i - 1, x_i + 1, y_i + 1], fill=255)
+
+#se llama a dibujar cada vez q se mantiene presionado el boton izq del mouse
+canvas.bind("<B1-Motion>", dibujar_con_mouse)
+
+# Botón Limpiar
+def limpiar_canvas():
+    canvas.delete("all")
+    dibujar.rectangle([0, 0, 28, 28], fill=0)
+
+#prediccion de la red
+def prediccion():
+    imagen_redimensionada = imagen.resize((28, 28))  # Asegúrate que esté en 28x28
+    img_array = np.array(imagen_redimensionada) / 255.0
+
+    # Expandir dimensiones para que tenga forma (1, 28, 28, 1)
+    img_array = np.expand_dims(img_array, axis=-1)
+    img_array = np.expand_dims(img_array, axis=0)
+
+    resultado = red.predict(img_array, verbose=0)
+    pred = np.argmax(resultado)
+
+    print("Predicción:", pred)
+
+tk.Button(ventana, text="Limpiar", command=limpiar_canvas).pack()
+tk.Button(ventana, text="Predecir", command=prediccion).pack()
+
+
+ventana.mainloop()
---
+++ red_neuronal.py
@@ -0,0 +1,72 @@
+from keras.utils import set_random_seed
+set_random_seed(394867)
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.datasets import mnist
+
+
+from keras import Sequential, Input
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+
+from keras.optimizers import SGD
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(-1, 28, 28, 1) / 255.0
+x_test = x_test.reshape(-1, 28, 28, 1) / 255.0
+
+# Atributos
+print(x_train)
+#Objetivos
+print( y_train)
+
+
+# Crear red 
+red = Sequential([
+    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)), # 32 filtros de 3*3 q hace la conv / imagen de 28x28 píxeles y 1 canal (b y n)
+    MaxPooling2D(pool_size=(2, 2)),    
+    
+    Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),  
+
+    Conv2D(64, kernel_size=(3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),   
+
+    Flatten(),                  # vector 3D -> vector 1D                         
+
+    Dense(128, activation='relu'),
+    Dropout(0.3),
+
+    Dense(64, activation='relu'),
+    Dropout(0.2),
+
+    Dense(10, activation='softmax')  # Calcula el número más probable
+])
+
+
+
+red.summary()
+
+# Compilar 
+optimizador = SGD(learning_rate=0.01)
+red.compile(
+    optimizer=optimizador,
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+#Entrena
+red.fit(
+    x_train,        # entrenamiento
+    y_train,         # objetivo
+    batch_size=64,                  # Tamaño del "lote" de entrenamiento
+    epochs=25,                     # Número de veces que se recorre todo el dataset
+    validation_split=0.1,           # Porcentaje de datos usados para validación
+    verbose=1                       # Nivel de detalle de la salida en consola
+)
+
+#Evalua
+red.evaluate(x_test, y_test)
+
+red.save('red.keras')
